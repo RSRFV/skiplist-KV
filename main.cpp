0a0bec6c380a5bcf02f5299a3592ab629320b240
用户姓名 <>
2024-08-05 10:27:22 +0800

    跳表核心实现

--- main.cpp
+++ main.cpp
@@ -3,34 +3,47 @@
 #define FILE_PATH "./store/dumpFile"
 
 int main() {
+    SkipList<std::string, std::string> skipList(5);
+    // insert 测试
+    std::cout << "--- insert 测试 ---" << std::endl;
+	skipList.insert_element("1", "one"); 
+    skipList.insert_element("2", "t"); 
+    skipList.insert_element("中文", " 测试2"); 
+	skipList.insert_element("english", "测试3"); 
 
-    // 键值中的key用int型，如果用其他类型，需要自定义比较函数
-    // 而且如果修改key的类型，同时需要修改skipList.load_file函数
-    SkipList<int, std::string> skipList(6);
-	skipList.insert_element(1, "这是"); 
-	skipList.insert_element(3, "一个"); 
-	skipList.insert_element(7, "以"); 
-	skipList.insert_element(8, "跳表"); 
-	skipList.insert_element(9, "为基础"); 
-	skipList.insert_element(19, "的KV"); 
-	skipList.insert_element(19, "存储引擎"); 
+    std::cout << "skipList size after insert_element(): " << skipList.size() << std::endl; // 4
+ 
+    // search 测试
+    std::cout << "--- search 测试 ---" << std::endl;
+    skipList.search_element("3");
+    skipList.search_element("中文");
+    skipList.search_element("4");
 
-    std::cout << "skipList size:" << skipList.size() << std::endl;
-
+    // delete 测试
+    std::cout << "--- delete 测试 ---" << std::endl;
+    skipList.delete_element("3");  // 成功
+    skipList.delete_element("5");  // 失败
+    std::cout << "skipList size after delete_element(): " << skipList.size() << std::endl; // 4
+   
+    // dump_file 测试
+    std::cout << "--- dump_file 测试 ---" << std::endl;
     skipList.dump_file();
 
-    // skipList.load_file();
+    // display 测试
+    std::cout << "--- display 测试 ---" << std::endl;
+    skipList.display_list();
+    
+    // clear 测试
+    std::cout << "--- clear 测试 ---" << std::endl;
+    skipList.clear();
+    std::cout << "skipList size after clear(): " << skipList.size() << std::endl; // 0
 
-    skipList.search_element(9);
-    skipList.search_element(18);
-
-
+    // load_file 测试
+    std::cout << "--- load_file 测试 ---" << std::endl;
+    skipList.load_file();
+    std::cout << "skipList size after load_file(): " << skipList.size() << std::endl; // 4
     skipList.display_list();
 
-    skipList.delete_element(3);
-    skipList.delete_element(7);
-
-    std::cout << "skipList size:" << skipList.size() << std::endl;
-
-    skipList.display_list();
+    return 0;
 }
+
